622aa273eddca3e985c085e164dbd1042b8e1b73
psajay-training <psajay.training@gmail.com>
2021-04-28 19:56:24 +0530

    Commit Module 1

--- coursera.py
+++ coursera.py
@@ -1,4 +1,12 @@
 '''
+psajay@psajay-ubuntu:~$ python3 -m http.server
+Serving HTTP on 0.0.0.0 port 8000 (http://0.0.0.0:8000/) ...
+
+psajay@psajay-ubuntu:~$ curl -o /dev/null -s -w "%{http_code}\n" http://localhost:8000
+200
+psajay@psajay-ubuntu:~$ curl -o /dev/null -s -w "%{http_code}\n" http://localhost:8000
+000
+-------------------------------
 x = 1
 x = x+1
 print (x)
@@ -263,4 +271,122 @@ elif score >= 0.6 :
     print ('D')
 elif score < 0.6 :
     print ('F')
+---------------------------
+# Functions -
+def thing():
+    print ("Hello")
+    print ('Fun')
+
+thing()
+print ('Zip')
+thing()
+---------------------------
+Type "help", "copyright", "credits" or "license" for more information.
+# first it will be all lowercase letters & then it will be uppercase of values
+
+>>> big = max ('Hello w World')
+>>> print (big)
+w
+>>> big = max ('Hello World')
+>>> print (big)
+r
+>>> big = max ('Hello world')
+>>> print (big)
+w
+
+>>> tiny = min ('Hello world')
+>>> print (tiny)
+
+# space is the lowest lowercase letter.
+------------------------------
+# defining a function
+x = 5
+print ('Hello')
+
+def print_lyrics ():
+    print ('i ''am a limberjack, and i''m okay.')
+    print ('i sleep all night & I work all day.')
+
+print ('Yo'), print_lyrics()
+x = x + 2
+print (x)
+------------------------------
+>>> def greet (lang) :
+...     if lang == 'es' :
+...         print ('Hola')
+...     elif lang == 'fr' :
+...         print ('bonjour')
+...     else :
+...         print ('Hello')
+...
+>>> greet ('en')
+Hello
+>>> greet ('es')
+Hola
+>>> greet ('fr')
+bonjour
+>>>
+>>> greet ('ab') # any random would be else
+Hello
+------------------------------
+
+def greet (lang) :
+    if lang == 'es' :
+        return 'Hola'
+    elif lang == 'fr' :
+        return 'bonjour'
+    else :
+        return 'Hello'
+# Output :
+>>> print (greet ('en'), 'glen')
+Hello glen
+>>> print (greet ('es'), 'sally')
+Hola sally
+>>> print (greet ('fr'), 'Michalel')
+bonjour Michalel
+# ------------------------------
+big = max ('Hello World')
+print (big)
+
+>>> big = max ('Hello World')
+>>> print (big)
+r
+>>> small = min ('Hello World')
+>>> print (small)
+
+>>> small = min ('Hello_World')
+>>> print (small)
+H
+------------------------------
+# Functions Sample
+def addtwo (a, b)
+    added = a + b
+    return added
+
+x = addtwo (3, 5)
+print (x)
+------------------------------
+4.6 Write a program to prompt the user for hours and rate per hour using input to compute gross pay.
+Pay should be the normal rate for hours up to 40 and time-and-a-half for the hourly rate for all hours worked above 40 hours.
+Put the logic to do the computation of pay in a function called computepay() and use the function to do the computation.
+The function should return a value. Use 45 hours and a rate of 10.50 per hour to test the program (the pay should be 498.75).
+You should use input to read a string and float() to convert the string to a number.
+Do not worry about error checking the user input unless you want to - you can assume the user types numbers properly.
+Do not name your variable sum or use the sum() function.
 '''
+def pay(h, r):
+    if h <= 40 :
+        print (h * r)
+    else :
+        print (h * 1.5)
+
+hrs = float(input("Enter Hours:"))
+rate = float(input("Enter Rate:"))
+pay(hrs, rate)
+
+print("Pay: ", pay)
+
+#pay = computepay(10,20)
+#print("Pay",pay)
+
+# pay = (hrs * (hrs - 40)) + (r * 40)
