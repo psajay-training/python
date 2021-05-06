afa2bae653a2f1596089edc0f976c01915c7f235
psajay-training <psajay.training@gmail.com>
2021-05-07 01:01:57 +0530

    Update - May 07

---
+++ Quick_One.py
@@ -0,0 +1,90 @@
+'''
+a= 10
+b = 1.5
+c = 'h'
+d = "Hello World"
+# print (d)
+type (c)
+f = 11/2
+print (f)
+print (int(f))
+-----------------------------
+and
+f and f f
+f and t f
+t and f f
+t and t t
+-----------------------------
+or
+t or t t
+t or f t
+f or t t
+f or f f
+-----------------------------
+not
+not t f
+not f t
+-----------------------------
+if (condition):
+    ------------------- will be executed
+else:
+    ------------------- will be executed
+# Sample :
+a = 6
+if (a == 5):
+    print ('yes')
+else:
+    print ('no')
+-----------------------------
+# sample and
+a = 5
+b = 10
+if ((a == 5) and b == 10):
+    print ('yes')
+else:
+    print ('no')
+-----------------------------
+# sample or
+a = 5
+b = 10
+if ((a == 5) or  b == 10):
+    print ('yes')
+else:
+    print ('no')
+-----------------------------
+# Sample not
+a = 5
+b = 10
+if (not True): # (True):
+    print ('yes')
+else:
+    print ('no')
+-----------------------------
+
+#  write a program to find max number of given :
+
+a = 10
+b = 20
+if (a>b):
+    print (a) # ("Max number is ", a)
+else :
+    print (b) # ("Max number is ", b)
+-----------------------------
+
+# Write a programme to find if the number is even or odd
+a = 6
+b = 2
+if (a % 2) == 0 :
+    print (a, "number is even")
+else:
+    print (a, "number is odd")
+-----------------------------
+'''
+# Write a programme to find if the number is even or odd
+a = input ('enter a number :')
+a = int (a)
+b = 2
+if (a % 2) == 0 :
+    print (a, "number is even")
+else:
+    print (a, "number is odd")
